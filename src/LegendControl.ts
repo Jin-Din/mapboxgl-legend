39dce01db6a911f97a6fce15a1c55c46bba93106
Jin <jdmaxt@qq.com>
2023-04-17 14:00:13 +0800

    添加了自定义容器

--- src/LegendControl.ts
+++ src/LegendControl.ts
@@ -1,10 +1,10 @@
-import './styles/main.scss';
-import { IControl } from 'mapbox-gl';
-import type { Map, Layer } from 'mapbox-gl';
-import components from './components';
-import expression from './expression';
-import { createElement } from './utils';
-import type { LayerOptions, LegendControlOptions } from './types';
+import "./styles/main.scss";
+import { IControl } from "mapbox-gl";
+import type { Map, Layer } from "mapbox-gl";
+import components from "./components";
+import expression from "./expression";
+import { createElement } from "./utils";
+import type { LayerOptions, LegendControlOptions } from "./types";
 
 export type { LayerOptions, LegendControlOptions };
 
@@ -17,34 +17,37 @@ const defaults: LayerOptions = {
 export default class LegendControl implements IControl {
   private _options: {
     layers: Record<string, LayerOptions> | undefined;
-  } & Omit<LegendControlOptions, 'layers'>;
-  
+  } & Omit<LegendControlOptions, "layers">;
+
   private _container: HTMLElement;
 
   private _map!: Map;
 
   constructor(options: LegendControlOptions = {}) {
-    const { layers, ...rest } = options;
+    const { container, layers, ...rest } = options;
+    container?.classList.add(...["mapboxgl-ctrl", "mapboxgl-ctrl-legend"]); //Jin Add
     this._options = { ...defaults, layers: undefined, ...rest };
     if (layers) this.addLayers(layers);
-    this._container = createElement('div', {
-      classes: ['mapboxgl-ctrl', 'mapboxgl-ctrl-legend'],
-    });
+    this._container =
+      container ||
+      createElement("div", {
+        classes: ["mapboxgl-ctrl", "mapboxgl-ctrl-legend"],
+      });
     this._loadPanes = this._loadPanes.bind(this);
   }
 
   onAdd(map: Map) {
     this._map = map;
-    this._map.on('styledata', this._loadPanes);
+    this._map.on("styledata", this._loadPanes);
     return this._container;
   }
 
   onRemove() {
     this._container.parentNode?.removeChild(this._container);
-    this._map?.off('styledata', this._loadPanes);
+    this._map?.off("styledata", this._loadPanes);
   }
 
-  addLayers(layers: NonNullable<LegendControlOptions['layers']>) {
+  addLayers(layers: NonNullable<LegendControlOptions["layers"]>) {
     const saveLayerOptions = (name: string, options: LayerOptions) => {
       const {
         collapsed = this._options.collapsed,
@@ -53,30 +56,48 @@ export default class LegendControl implements IControl {
         onToggle = this._options.onToggle,
         attributes,
       } = options;
-      this._options.layers![name] = { collapsed, toggler, highlight, onToggle, attributes };
+      this._options.layers![name] = {
+        collapsed,
+        toggler,
+        highlight,
+        onToggle,
+        attributes,
+      };
     };
-    
+
     this._options.layers ??= {};
-    if (Array.isArray(layers)) layers.forEach(name => saveLayerOptions(name, {}));
-    else Object.entries(layers).forEach(([name, options]) => {
-      if (typeof options === 'boolean') saveLayerOptions(name, {});
-      else if (Array.isArray(options)) saveLayerOptions(name, { attributes: options });
-      else saveLayerOptions(name, options); 
-    });
-    
+    if (Array.isArray(layers))
+      layers.forEach((name) => saveLayerOptions(name, {}));
+    else
+      Object.entries(layers).forEach(([name, options]) => {
+        if (typeof options === "boolean") saveLayerOptions(name, {});
+        else if (Array.isArray(options))
+          saveLayerOptions(name, { attributes: options });
+        else saveLayerOptions(name, options);
+      });
+
     if (this._map?.isStyleLoaded()) this._loadPanes();
   }
 
   removeLayers(layerIds: string[]) {
-    layerIds.forEach(id => {
+    layerIds.forEach((id) => {
       delete this._options.layers?.[id];
-      const pane = this._container.querySelector(`.mapboxgl-ctrl-legend-pane--${id}`);
+      const pane = this._container.querySelector(
+        `.mapboxgl-ctrl-legend-pane--${id}`
+      );
       if (pane) this._container.removeChild(pane);
     });
   }
-
+  /**
+   * [自定义扩展] 返回已加载的图层id
+   * @returns
+   */
+  getLayers(): string[] {
+    const { layers } = this._options;
+    return layers ? Object.keys(layers).map((item) => item) : [];
+  }
   private _getBlock(key: string, layer: Layer, attribute: string, value: any) {
-    const [property] = attribute.split('-').slice(-1);
+    const [property] = attribute.split("-").slice(-1);
     const component = components[property as keyof typeof components];
     if (!component) return;
     const parsed = expression.parse(value);
@@ -85,50 +106,69 @@ export default class LegendControl implements IControl {
   }
 
   private _toggleButton(layerId: string, key: string) {
-    const { onToggle = this._options.onToggle } = this._options.layers?.[key] || {};
-    const visibility = this._map?.getLayoutProperty(layerId, 'visibility') || 'visible';
-    const button = createElement('div', { classes: ['toggler', `toggler--${visibility}`] });
-    button.addEventListener('click', event => {
+    const { onToggle = this._options.onToggle } =
+      this._options.layers?.[key] || {};
+    const visibility =
+      this._map?.getLayoutProperty(layerId, "visibility") || "visible";
+    const button = createElement("div", {
+      classes: ["toggler", `toggler--${visibility}`],
+    });
+    button.addEventListener("click", (event) => {
       event.preventDefault();
-      const visible = visibility === 'none' ? 'visible' : 'none';
-      this._map?.setLayoutProperty(layerId, 'visibility', visible);
-      onToggle?.(layerId, visible === 'visible');
+      const visible = visibility === "none" ? "visible" : "none";
+      this._map?.setLayoutProperty(layerId, "visibility", visible);
+      onToggle?.(layerId, visible === "visible");
     });
     return button;
   }
 
   private _loadPanes() {
     const layersIds = Object.keys(this._options.layers || {});
-    this._map.getStyle().layers
-      .filter(layer => (layer as Layer).source && (layer as Layer).source !== 'composite')
-      .filter(layer => !this._options.layers || layersIds.some(name => layer.id.match(name)))
+    this._map
+      .getStyle()
+      .layers.filter(
+        (layer) =>
+          (layer as Layer).source && (layer as Layer).source !== "composite"
+      )
+      .filter(
+        (layer) =>
+          !this._options.layers ||
+          layersIds.some((name) => layer.id.match(name))
+      )
       .reverse() // Show in order that are drawn on map (first layers at the bottom, last on top)
-      .forEach(layer => {
+      .forEach((layer) => {
         const { id, layout, paint, metadata } = layer as Layer;
-        const key = layersIds.find(name => id.match(name)) || id;
-        const { collapsed, toggler, attributes } = this._options.layers?.[key] || this._options;
+        const key = layersIds.find((name) => id.match(name)) || id;
+        const { collapsed, toggler, attributes } =
+          this._options.layers?.[key] || this._options;
 
         // Construct all required blocks, break if none
-        const paneBlocks = Object.entries({ ...layout, ...paint })
-          .reduce((acc, [attribute, value]) => {
+        const paneBlocks = Object.entries({ ...layout, ...paint }).reduce(
+          (acc, [attribute, value]) => {
             const visible = attributes?.includes(attribute) ?? true;
             if (!visible) return acc;
             const block = this._getBlock(key, layer, attribute, value);
             if (block) acc.push(block);
             return acc;
-          }, [] as HTMLElement[]);
+          },
+          [] as HTMLElement[]
+        );
         if (!paneBlocks.length) return;
 
         // (re)Construct pane and replace (if already exist)
         const selector = `mapboxgl-ctrl-legend-pane--${id}`;
         const prevPane = this._container.querySelector(`.${selector}`);
-        const pane = createElement('details', {
-          classes: ['mapboxgl-ctrl-legend-pane', selector],
-          attributes: { open: prevPane ? prevPane.getAttribute('open') !== null : !collapsed },
+        const pane = createElement("details", {
+          classes: ["mapboxgl-ctrl-legend-pane", selector],
+          attributes: {
+            open: prevPane
+              ? prevPane.getAttribute("open") !== null
+              : !collapsed,
+          },
           content: [
-            createElement('summary', {
+            createElement("summary", {
               content: [
-                metadata?.name || id,
+                metadata?.name || this._formatLabel(id),
                 toggler && this._toggleButton(id, key),
               ],
             }),
@@ -138,5 +178,23 @@ export default class LegendControl implements IControl {
         if (prevPane) this._container.replaceChild(pane, prevPane);
         else this._container.appendChild(pane);
       });
+  }
+
+  //Jin modify 格式化显示的label，去除prefix
+  private _formatLabel(id: string) {
+    if (this._options.labelPrefix) {
+      let labelPrefix: string | string[] = this._options.labelPrefix;
+      if (typeof labelPrefix === "string") {
+        return id.startsWith(labelPrefix) ? id.replace(labelPrefix, "") : id;
+      }
+      // debugger;
+      let labelPrefixs = labelPrefix as string[];
+      // console.log(labelPrefixs);
+      let hit = labelPrefixs.find((prefix) => id.startsWith(prefix));
+      if (hit) return id.replace(hit, "");
+
+      return id;
+    }
+    return id;
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import type { Map, Layer, Expression, ExpressionName } from 'mapbox-gl';
+import type { Map, Layer, Expression, ExpressionName } from "mapbox-gl";
 
 export type { Map, Layer, Expression, ExpressionName };
 
@@ -7,16 +7,18 @@ export type LayerOptions = {
   toggler?: boolean;
   attributes?: string[];
   highlight?: boolean;
+  container?: HTMLElement; //Jin add.指定容器。如果为空则为默认创建，反之，加载到指定的容器。如果container不为空，则外部需手动调用onAdd,而不是用map.addControl
+  labelPrefix?: string | string[]; //Jin add.图例名前缀，如果指定，则在图例显示时会将前缀删除掉.
   onToggle?: (layer: string, visibility: boolean) => void;
 };
 
 export type LegendControlOptions = {
-  layers?: string[] | Record<string, boolean | string[] | LayerOptions>
+  layers?: string[] | Record<string, boolean | string[] | LayerOptions>;
 } & LayerOptions;
 
 export type ParsedExpression<In, Out> = {
   name: ExpressionName;
-  getter: Expression | undefined,
+  getter: Expression | undefined;
   stops: [In, Out][];
   inputs: In[];
   outputs: Out[];
